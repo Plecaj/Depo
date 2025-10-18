c7d35ed4bf0f6e45de71c6e87adab1e42b0f0f7a
Maria Majchrzak <maniulka209@gmail.com>
2025-10-18 11:09:58 +0200

    HelpBar style, icon

--- GUI/src/components/HelpBar/HelpBar.jsx
+++ GUI/src/components/HelpBar/HelpBar.jsx
@@ -1,10 +1,14 @@
 import styles from'./HelpBar.module.css';
+import icon from '../../assets/icon.png'
 
 function HelpBar() {
 
     return(
         <>
-            <div className={styles.bar}> . </div>
+            <div className={styles.bar}>
+                <img src={icon} alt="icon" className={styles.icon}></img>
+                <div className={styles.line}>.</div>
+            </div>
         </>
     );
 }
