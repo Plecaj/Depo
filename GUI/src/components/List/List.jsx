cfba891243765e1efa32cc95da7faf0097629543
Maria Majchrzak <maniulka209@gmail.com>
2025-10-18 17:45:38 +0200

    fetching data from rust, and selecting the project path

--- GUI/src/components/List/List.jsx
+++ GUI/src/components/List/List.jsx
@@ -1,27 +1,27 @@
 import icon from '../../assets/delete.png'
 import styles from './List.module.css'
 import { PackagesData } from '../../App.jsx';
-import {useContext} from "react";
+import {useContext, useEffect} from "react";
+import { invoke } from "@tauri-apps/api/core";
 
 function List() {
 
-    const [packages] = useContext(PackagesData);
+    const {setPackageData} = useContext(PackagesData);
+    const {packageData} = useContext(PackagesData);
+    const {path} = useContext(PackagesData);
 
     return(
         <div className={styles.list}>
-            {
-                packages.map(pkg => {
-                    return(
-                        <div className={styles.elements}
-                             key={pkg.name}> {pkg.name} : {pkg.version}
-                            - {pkg.installed ?  "yes" : "no"}
-                            <div className={styles.delete}>
-                                <img src={icon} alt="delete" ></img>
-                            </div>
+            {packageData &&   Object.values(packageData).map(pkg => {
+                return(
+                    <div className={styles.elements}
+                         key={pkg.name}> {pkg.name}
+                        <div className={styles.delete}>
+                            <img src={icon} alt="delete" ></img>
                         </div>
-                    );
-                })
-            }
+                    </div>
+                );
+            })}
         </div>
     );
 }
--- GUI/src/components/SelectProject/SelectProject.jsx
+++ GUI/src/components/SelectProject/SelectProject.jsx
@@ -1,14 +1,28 @@
 import styles from'./SelectProject.module.css';
 import { open } from "@tauri-apps/plugin-dialog";
+import { invoke } from "@tauri-apps/api/core";
+import {useContext} from "react";
+import {PackagesData} from "../../App.jsx";
 
 function SelectProject() {
 
-    async function select() {
-        const okok = await open({
+    const {setPath} = useContext(PackagesData);
+    const {fetchData} = useContext(PackagesData);
+
+    async function select(){
+        let path = await open({
             multiple: false,
             directory: true,
         });
-       console.log(okok);
+        path = path + "\\package.yaml";
+        try{
+            await invoke('get_project_deps', {path: path});
+            console.log("selected project :" + path);
+            setPath(path);
+            fetchData();
+        }catch(e){
+            console.log("somthing went wrong! with selecting project path : " + e);
+        }
     }
 
     return(
